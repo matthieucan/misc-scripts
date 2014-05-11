ffaff7bb3ef2c7d8778039f2393b8b9798bcf027
Matthieu Caneill <matthieu.caneill42@gmail.com>
2014-05-11 19:40:33 -0400

    add coccinelle

--- post-install.sh
+++ post-install.sh
@@ -5,7 +5,7 @@
 apt-get install -y emacs git virtualbox python-dev pip virtualenv ipython \
     vlc guake awesome awesome-extra terminator chromium gitk flashplugin-nonfree \
     volti colordiff pidgin pidgin-otr pidgin-skype pidgin-twitter \
-    xscreensaver xscreensaver-data
+    xscreensaver xscreensaver-dat coccinelle
 
 # do as user
 
