f579bbe8828d26610f9d3c95567868bf107375c7
Matthieu Caneill <matthieu.caneill42@gmail.com>
2014-05-10 20:48:54 -0400

    add stuff

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+ #!/bin/bash
 
 # run as root
 
 apt-get install -y emacs git virtualbox python-dev pip virtualenv ipython \
-    vlc guake awesome awesome-extra terminator chromium gitk
+    vlc guake awesome awesome-extra terminator chromium gitk flashplugin-nonfree \
+    volti colordiff pidgin pidgin-otr pidgin-skype pidgin-twitter
+
+# do as user
+
+# git config --global user.name foo
+# git config --global user.email bar
