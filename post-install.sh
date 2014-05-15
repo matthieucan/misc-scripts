9a718ae5fc5bfad86c971c08bf4f1b1e555e5f16
Matthieu Caneill <matthieu.caneill42@gmail.com>
2014-05-15 19:02:12 -0400

    add docker

--- post-install.sh
+++ post-install.sh
@@ -3,4 +3,4 @@
 # run as root
 
 apt-get install -y emacs git virtualbox python-dev pip virtualenv ipython \
-    vlc guake awesome awesome-extra terminator chromium gitk
+    vlc guake awesome awesome-extra terminator chromium gitk docker.io
