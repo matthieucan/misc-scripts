05508cba0beb676adfb904a49e7a98e9afc57a85
Matthieu Caneill <matthieu.caneill42@gmail.com>
2014-05-26 19:48:36 -0400

    add texlive to post install

--- post-install.sh
+++ post-install.sh
@@ -6,7 +6,8 @@ apt-get install -y emacs git virtualbox python-dev pip virtualenv ipython \
     vlc guake awesome awesome-extra terminator chromium gitk
     vlc guake awesome awesome-extra terminator chromium gitk flashplugin-nonfree \
     volti colordiff pidgin pidgin-otr pidgin-skype pidgin-twitter \
-    xscreensaver xscreensaver-data coccinelle docker.io python-mode
+    xscreensaver xscreensaver-data coccinelle docker.io python-mode \
+    texlive
 
 # do as user
 
