4c3bfb9bb62ab66ac552463ce2a7475f7e4da373
Matthieu Caneill <matthieu.caneill42@gmail.com>
2015-07-02 22:42:24 +0200

    add owncloud

--- post-install.sh
+++ post-install.sh
@@ -23,7 +23,7 @@ apt-get install -y \
     mpd ncmpcpp weather-util htop blueman pulseaudio openjdk-7-jdk \
     mutt-patched sl ntp libreoffice \
     cvs subversion mercurial automake autoconf emacs24-el gtk-theme-switch \
-    zsh ranger tmux tree mosh
+    zsh ranger tmux tree mosh owncloud-client
 
 gem install jekyll
 gem install jekyll-scholar
