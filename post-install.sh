bb09ad4b91f4bea2a971be9c38587a972ed3d0e2
Matthieu Caneill <matthieu.caneill42@gmail.com>
2015-09-02 19:56:25 +0200

    add offlineimap

--- post-install.sh
+++ post-install.sh
@@ -21,7 +21,7 @@ apt-get install -y \
     golang-go gocode-auto-complete-el golang-mode \
     eog file-roller reaver wifite aircrack-ng \
     mpd ncmpcpp weather-util htop blueman pulseaudio openjdk-7-jdk \
-    mutt-patched sl ntp libreoffice \
+    mutt-patched offlineimap sl ntp libreoffice \
     cvs subversion mercurial automake autoconf emacs24-el gtk-theme-switch \
     zsh ranger tmux tree mosh owncloud-client offlineimap macchanger parcellite
 
