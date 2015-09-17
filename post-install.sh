2804d73b2d7894b7057778843acbeffb221ca597
Matthieu Caneill <matthieu.caneill@imag.fr>
2015-09-17 14:41:48 +0200

    add npm

--- post-install.sh
+++ post-install.sh
@@ -23,7 +23,8 @@ apt-get install -y \
     mpd ncmpcpp weather-util htop blueman pulseaudio openjdk-7-jdk \
     mutt-patched sl ntp libreoffice \
     cvs subversion mercurial automake autoconf emacs24-el gtk-theme-switch \
-    zsh ranger tmux tree mosh owncloud-client offlineimap macchanger parcellite
+    zsh ranger tmux tree mosh owncloud-client offlineimap macchanger parcellite \
+    npm
 
 gem install jekyll
 gem install jekyll-scholar
