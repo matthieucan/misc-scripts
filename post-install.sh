20b2141f4437d3d2e865c16f8f375789d6a48f72
Matthieu Caneill <matthieu.caneill@imag.fr>
2016-02-25 14:32:16 +0100

    add xetex

--- post-install.sh
+++ post-install.sh
@@ -16,7 +16,7 @@ apt-get install -y \
     pidgin pidgin-otr pidgin-skype pidgin-twitter gimp \
     evince transmission vlc terminator chromium iceweasel flashplugin-nonfree \
     colordiff coccinelle docker.io pandoc \
-    texlive latex-make parcimonie \
+    texlive latex-make texlive-xetex parcimonie \
     sloccount  signing-party devscripts python-stdeb dh-make dpkg-dev-el debian-el \
     golang-go gocode-auto-complete-el golang-mode \
     eog file-roller reaver wifite aircrack-ng \
